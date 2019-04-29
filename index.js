06f5d4b2025b2566e1d3787d8a701d9c5515ee91
Renan Ferreira <renan@inventae.com.br>
2019-04-29 18:53:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const { BadRequest } = require('http-errors')
+const fp = require('fastify-plugin')
+
+function parseQuery(query, fastify) {
+	if (typeof query == 'string') {
+		query = JSON.parse(query)
+	}
+
+	Object.keys(query)
+		.forEach(k => {
+			if (k == 'include' && typeof query[k] != 'string') {
+				if (Array.isArray(query[k])) {
+					query[k].map(e => parseInclude(e, fastify))
+				} else {
+					query[k] = parseInclude(query[k], fastify)
+				}
+			}
+		})
+	return query
+}
+
+function parseInclude(include, fastify) {
+	Object.keys(include)
+		.forEach(k => {
+			if (k == 'model') {
+				let model = fastify.sequelize.models[include[k]]
+				console.log(`${include[k]}`, model)
+				if (!!model) {
+					include[k] = model
+				} else {
+					const models = Object.keys(fastify.sequelize.models)
+					const meant = models.find(m => m.toLowerCase() == include[k].toLowerCase())
+					throw new Error(`Model ${include[k]} does not exist. ${meant ? `Did you mean '${meant}'?` : ''}`)
+				}
+			} else if (k == 'include' && typeof include[k] != 'string') {
+				if (Array.isArray(include[k])) {
+					include[k].forEach(e => parseInclude(e, fastify))
+				} else {
+					include[k] = parseInclude(include[k], fastify)
+				}
+			}
+		})
+	return include
+}
+
+
+module.exports = fp(function (fastify, opts, next) {
+	fastify.addHook('onRequest', function (request, reply, next) {
+		if (request.query.q) {
+			try {
+				request.query.q = parseQuery(request.query.q, fastify)
+			} catch (e) {
+				const err = new BadRequest(e)
+				return reply.code(err.statusCode).send(err)
+			}
+		}
+		console.log("Parsed query", request.query.q)
+		return next()
+	})
+	return next()
+})
